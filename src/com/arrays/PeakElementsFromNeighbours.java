39cddd9a1ffe1a53936a1133c748ebaeb9923fe1
Purna Brahma Sahu <hello.pbs2027@gmail.com>
2024-06-25 13:36:35 +0530

    Update PeakElementsFromNeighbours.java

--- src/com/arrays/PeakElementsFromNeighbours.java
+++ src/com/arrays/PeakElementsFromNeighbours.java
@@ -57,7 +57,7 @@ public class PeakElementsFromNeighbours {
 		
 		
 		for (int i=1;i<size-1;i++) {
-			if(arr[i] >=arr[i-1] && arr[i]> arr[i+1]) {
+			if(arr[i] >=arr[i-1] && arr[i]>= arr[i+1]) {
 				peakArr.add(arr[i]);
 			}
 		}
